8bb00626a1e9ed1385b1cb89f87e637bc6dee61d
Ty1er47 <111007514+Ty1er47@users.noreply.github.com>
2022-09-12 11:28:57 -0700

    Code

---
+++ Text Game-project 3/Assets/Scripts/Sceneloader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Sceneloader : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // Update is called once per frame
+    public void LoadStartScene()
+    {
+       SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
